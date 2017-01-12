f67573f50b04c6c711f2feec4542983851e73215
danieljbell <djbell70@gmail.com>
2017-01-12 11:37:20 -0500

    first draft complete

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bonus Content for 17 Sales Trends for 2017 Whitepaper
+
+- git clone https://github.com/danieljbell/Sales-Trends-bonus-content.git
+- run `npm install`
+- run `gulp`
+
+To make edits, make sure to link the css file in the head and remove inline styles on lines 149-151
+`<link href="css/style.css">`
+
+After edits are made, make sure to inline the styles again and remove the css link from the head. Then you can push to the page on wordpress.
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+var gulp          = require('gulp');
+var browserSync   = require('browser-sync').create();
+var sass          = require('gulp-sass');
+var postcss       = require('gulp-postcss');
+var atImport      = require('postcss-import');
+var autoprefixer  = require('autoprefixer');
+var mqpacker      = require('css-mqpacker');
+var cssnano       = require('cssnano');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -15,10 +20,17 @@ gulp.task('serve', ['sass'], function() {
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("css"))
-        .pipe(browserSync.stream());
+  var processors = [
+    atImport,
+    autoprefixer({browsers: ['last 6 versions']}),
+    mqpacker,
+    cssnano
+  ];
+  return gulp.src("scss/*.scss")
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(processors))
+    .pipe(gulp.dest("css"))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve']);
